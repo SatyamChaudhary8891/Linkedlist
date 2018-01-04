cf13d1a94785cd0cb58f33f58951fd2e3a04309b
SatyamChaudhary8891 <satyamchaudhary8891@gmail.com>
2018-01-04 23:05:15 +0530

    added algorithms for linkedlist data structure

--- linkedlist.js
+++ linkedlist.js
@@ -121,6 +121,126 @@ LinkedList.prototype.findMiddle = function () {
     return slow_node.value;
 };
 
+/**
+ * To find kth node from the reverse order of linkedlist
+ * @param k
+ * @returns {null|*}
+ */
+LinkedList.prototype.getNodeFromLast = function(k) {
+    var f_pointer = this.head;
+    var count = 0;
+    var s_pointer = this.head;
+    while(f_pointer !== null){
+        if(count < k){
+            count++;
+            f_pointer = f_pointer.next;
+        }else{
+            f_pointer = f_pointer.next;
+            s_pointer = s_pointer.next;
+        }
+    }
+    return s_pointer;
+};
+
+/**
+ * To reverse linkedlist pointers via recursion
+ * @param node
+ * @param prev
+ * @returns {*}
+ */
+LinkedList.prototype.reverseLinkedList = function (node, prev) {
+    if(node === null) return prev;
+    var next = node.next;
+    var curt = node;
+    curt.next = prev;
+    return LinkedList.prototype.reverseLinkedList.call(null,next, curt);
+};
+
+/**
+ * To reverse linkedlist pointers via loop
+ *
+ * @returns {*}
+ */
+LinkedList.prototype.reverseLinkedList2 = function () {
+    var next = null;
+    var curt = this.head;
+    var prev = null;
+    while(curt !== null){
+        next = curt.next;
+        curt.next = prev;
+        prev = curt;
+        curt = next;
+    }
+    return prev;
+};
+
+/**
+ * To reverse alternative kth node
+ * @param node
+ * @param k
+ * @returns {*}
+ */
+LinkedList.prototype.reverseKNode = function (node, k) {
+    var curt = node;
+    var prev = null;
+    var next = null;
+    var count = 0;
+    while (curt !== null && count < k){
+        next = curt.next;
+        curt.next = prev;
+        prev = curt;
+        curt = next;
+        count++;
+    }
+    if(next !== null)
+        node.next = LinkedList.prototype.reverseKNode(curt, k);
+    return prev;
+};
+
+/**
+ * To detect the loop in linkedlist.
+ * @returns {boolean}
+ */
+LinkedList.prototype.detectLoop = function () {
+    var f_Pointer = this.head;
+    var s_Pointer = this.head;
+    while (s_Pointer !== null && s_Pointer.next !== null){
+        f_Pointer = f_Pointer.next;
+        s_Pointer = s_Pointer.next.next;
+        if(f_Pointer === s_Pointer){
+            break;
+        }
+    }
+    if(f_Pointer === s_Pointer)
+        return true;
+    else
+        return false;
+};
+
+
+LinkedList.prototype.mergeTwoSortedList = function (list2) {
+    var list1 = this.head;
+    if(list1 === null) return list2;
+    if(list2 === null) return list1;
+    if(list1 === list2) return list1;
+    var rs = new LinkedList();
+    var node1 = list1;
+    var node2 = list2;
+    console.log(list2, list1);
+    while (node1 !== null && node2 !== null){
+        if(node1.value < node2.value){
+            rs.addNode(node1.value);
+            node1 = node1.next;
+        }else{
+            rs.addNode(node2.value);
+            node2 = node2.next;
+        }
+    }
+    console.log(rs, node1, node2);
+    return rs;
+};
+
+
 function main() {
     let list = new LinkedList();
     list.addNode("Gpapa");
@@ -129,7 +249,30 @@ function main() {
     list.addNode("Gson");
     list.addNode("Thankyou");
     list.traverse();
+    /*
     list.reverseTraverse(list.head);
     console.log('Middle of list - ',list.findMiddle());
+    console.log('3 Node from last - ', list.getNodeFromLast(3));
+    console.log('Reverse via recursion approach - ',list.reverseLinkedList(list.head, null));
+    console.log('Reverse via loop approach - ',list.reverseLinkedList2());
+    console.log('Reverse Alternative 2 node - ', list.reverseKNode(list.head, 2));
+    console.log('checked loop - ',list.detectLoop());
+    */
+    let list1 = new LinkedList();
+    list1.addNode(4);
+    list1.addNode(8);
+    list1.addNode(10);
+    list1.addNode(12);
+    list1.addNode(20);
+
+    let list2 = new LinkedList();
+    list2.addNode(1);
+    list2.addNode(8);
+    list2.addNode(9);
+    list2.addNode(13);
+    list2.addNode(19);
+    console.log(list1, list2);
+    let rs = list1.mergeTwoSortedList(list2.head);
+    rs.traverse();
 }
 main();
